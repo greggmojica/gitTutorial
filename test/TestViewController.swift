e263cc3f96f3969cd9926964952964c7e7bea55f
Gregg Mojica <GreggMojica@MacBook-12.home>
2016-05-10 21:52:30 -0400

    added testvc

---
+++ test/TestViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TestViewController.swift
+//  test
+//
+//  Created by Gregg Mojica on 5/10/16.
+//  Copyright © 2016 Gregg Mojica. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.redColor()
+    }
+}
